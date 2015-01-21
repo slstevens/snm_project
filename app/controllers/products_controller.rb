09af998a45d4858695ffb799bf34fab11be182c6
Sean Stevens <sean.stevens.123@gmail.com>
2015-01-21 16:24:19 +0000

    Added a product show page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,20 @@ class ProductsController < ApplicationController
 		@products = Product.all
 	end
 
+	def new
+		@product = Product.new
+	end
+
+	def create
+		Product.create(product_params)
+		redirect_to products_path
+	end
+
+	def show
+		@product = Product.find(params[:id])
+	end
+
+	def product_params
+		params.require(:product).permit(:name, :description, :price)
+	end
 end
