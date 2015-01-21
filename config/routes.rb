a2c9a11f9a64deff9338946f83bb524d22e76ef6
Sean Stevens <sean.stevens.123@gmail.com>
2015-01-21 18:36:54 +0000

    Added user login and facebook auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  devise_scope :user do
+    get 'sign_out', :to => 'devise/sessions#destroy', :only => :destroy_user_session
+  end
+
+  root to: "products#index"
+
   resources :products do
     resources :comments
   end
